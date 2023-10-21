43d027aa4e4653f73ab83b98badf4ee2e9290ec2
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-10-21 00:19:24 -0700

    Fix seed data and seed.js. All posts have the right user and all comments belong to the right post

--- seeds/seed.js
+++ seeds/seed.js
@@ -16,31 +16,22 @@ const seedDatabase = async () => {
     await sequelize.sync({ force: true });
 
     // Insert user data from userData.json into the User table
-    const createdUsers = await User.bulkCreate(userData, {
+    await User.bulkCreate(userData, {
       individualHooks: true, // Apply hooks for password hashing
       returning: true, // Return the created user records
     });
 
     // Insert post data from postData.json into the Post table
-    const createdPosts = await Post.bulkCreate(postData, {
+    await Post.bulkCreate(postData, {
       returning: true, // Return the created post records
     });
 
     // Create comments and associate them with users and posts
     for (const comment of commentData) {
-      const randomUser =
-        createdUsers[Math.floor(Math.random() * createdUsers.length)];
-      const randomPost =
-        createdPosts[Math.floor(Math.random() * createdPosts.length)];
-
-      await Comment.create({
-        ...comment,
-        user_id: randomUser.id,
-        post_id: randomPost.id,
-      });
+      await Comment.create(comment);
     }
 
-    console.log("Database seeding completed successfully! 🌱");
+    console.log(" 🌱  Database seeding completed successfully!  🌱  ");
     return true; // Indicate success
   } catch (error) {
     console.error("Error during seeding:", error.message); // Provide a more meaningful error message
